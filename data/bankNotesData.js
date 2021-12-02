962a91de54ed1e9d8b715c0e265bae9c5ebb932a
YlN <thiebaut.jocelyn@gmail.com>
2021-12-02 19:43:08 +0100

    Hello, Git!

---
+++ data/bankNotesData.js
@@ -0,0 +1,283 @@
+var bankNotesData = {
+    "euro": {
+        "symbol": "€",
+        "fontAwesome": "fas fa-euro-sign",
+        "source": "https://fr.wikipedia.org/wiki/Billets_de_banque_en_euros#Caract%C3%A9ristiques_individuelles_2",
+        "bankNotes": {
+            "5": {
+                "value": 5,
+                "width": 12.0,
+                "height": 6.2,
+                "thickness": 0.12,
+                "weight": 0.6
+            },
+            "10": {
+                "value": 10,
+                "width": 12.7,
+                "height": 6.7,
+                "thickness": 0.12,
+                "weight": 0.7
+            },
+            "20": {
+                "value": 20,
+                "width": 13.3,
+                "height": 7.2,
+                "thickness": 0.12,
+                "weight": 0.8
+            },
+            "50": {
+                "value": 50,
+                "width": 14.0,
+                "height": 7.7,
+                "thickness": 0.12,
+                "weight": 0.9
+            },
+            "100": {
+                "value": 100,
+                "width": 14.7,
+                "height": 8.2,
+                "thickness": 0.12,
+                "weight": 1.0
+            },
+            "200": {
+                "value": 200,
+                "width": 15.3,
+                "height": 8.2,
+                "thickness": 0.12,
+                "weight": 1.1
+            },
+            "500": {
+                "value": 500,
+                "width": 16.0,
+                "height": 8.2,
+                "thickness": 0.12,
+                "weight": 1.1
+            }
+        }
+    },
+    "dollar": {
+        "symbol": "$",
+        "source": "TODO",
+        "fontAwesome": "fas fa-dollar-sign",
+        "bankNotes": {
+            "1": {
+                "value": 1,
+                "width": 15.5955,
+                "height": 6.6294,
+                "thickness": 0.11,
+                "weight": 0.992
+            },
+            "5": {
+                "value": 5,
+                "width": 15.5956,
+                "height": 6.6294,
+                "thickness": 0.11,
+                "weight": 0.992
+            },
+            "10": {
+                "value": 10,
+                "width": 15.5955,
+                "height": 6.6294,
+                "thickness": 0.11,
+                "weight": 0.992
+            },
+            "20": {
+                "value": 20,
+                "width": 15.5955,
+                "height": 6.6294,
+                "thickness": 0.11,
+                "weight": 0.992
+            },
+            "50": {
+                "value": 50,
+                "width": 15.5955,
+                "height": 6.6294,
+                "thickness": 0.11,
+                "weight": 0.992
+            },
+            "100": {
+                "value": 100,
+                "width": 15.5955,
+                "height": 6.6294,
+                "thickness": 0.11,
+                "weight": 0.992
+            },
+
+        }
+    }
+};
+
+
+
+var valueComparison = {
+    length: {
+        baseUnit: "mm",
+        unit: {
+            mm: 1,
+            cm: 10,
+            m: 1000,
+            km: 1000000
+        },
+        comparison: [{
+                value: 1,
+                name: "TODO",
+                img: "TODO",
+            }, {
+                value: 5,
+                name: "Ant",
+                img: "https://upload.wikimedia.org/wikipedia/commons/7/70/The-Ant.jpg",
+                attribution: "Abdulmominbd, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+            },
+            {
+                value: 12,
+                name: "Bee",
+                img: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Apis_mellifera_Western_honey_bee.jpg",
+                attribution: "Andreas Trepte, CC BY-SA 2.5 <https://creativecommons.org/licenses/by-sa/2.5>, via Wikimedia Commons"
+            },
+            {
+                value: 32,
+                name: "SD Card",
+                img: "https://upload.wikimedia.org/wikipedia/commons/1/13/2015_Karta_microSD_z_adapterem_SD.jpg",
+                attribution: "Jacek Halicki, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+            },
+            {
+                value: 85,
+                name: "Apple",
+                img: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Honeycrisp.jpg",
+                attribution: "Jonathunder, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 450,
+                name: "Rabbit",
+                img: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Domestic_white_rabbit_sitting_in_a_meadow.jpg",
+                attribution: "Niftypen, CC BY 4.0 <https://creativecommons.org/licenses/by/4.0>, via Wikimedia Commons"
+            },
+            {
+                value: 550,
+                name: "Golden Retriever",
+                img: "https://upload.wikimedia.org/wikipedia/commons/0/01/Golden_retriever_ruede.jpg",
+                attribution: "Dirk Vorderstraße, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 1700,
+                name: "Mankind",
+                img: "https://cdn.pixabay.com/photo/2018/12/01/10/44/silhouette-3849284_960_720.png",
+                attribution: ""
+            },
+            {
+                value: 3000,
+                name: "Elephant",
+                img: "https://upload.wikimedia.org/wikipedia/commons/2/22/Elephant_side-view_Kruger.jpg",
+                attribution: "Felix Andrews (Floybix), CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons"
+            },
+            {
+                value: 4400,
+                name: "Double deck bus",
+                img: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Double_Decker_Bus_in_London.jpg",
+                attribution: "Nihcolas Babaian, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+            },
+            {
+                value: 5880,
+                name: "Giraffe",
+                img: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Giraffa_camelopardalis_reticulata_01.JPG",
+                attribution: "H. Zell, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 20000,
+                name: "Sphinx of Giza",
+                img: "https://upload.wikimedia.org/wikipedia/commons/f/f6/Great_Sphinx_of_Giza_-_20080716a.jpg",
+                attribution: "English:  Taken by the uploader, w:es:Usuario:BarcexEspañol:  Tomada por w:es:Usuario:Barcex, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons"
+            },
+            {
+                value: 33600,
+                name: "Whale",
+                img: "https://upload.wikimedia.org/wikipedia/commons/1/1c/Anim1754_-_Flickr_-_NOAA_Photo_Library.jpg",
+                attribution: "NOAA Photo Library, Public domain, via Wikimedia Commons"
+            },
+            {
+                value: 50000,
+                name: "Arc de Triomphe",
+                img: "https://upload.wikimedia.org/wikipedia/commons/e/e9/Arc_de_triomphe_Paris.jpg",
+                attribution: "I, Sese Ingolstadt, CC BY-SA 2.5 <https://creativecommons.org/licenses/by-sa/2.5>, via Wikimedia Commons"
+            },
+            {
+                value: 93000,
+                name: "Statue of Liberty",
+                img: "https://upload.wikimedia.org/wikipedia/commons/8/85/Statue_of_liberty_01.jpg",
+                attribution: "Francisco Diez from New York City, USA, CC BY 2.0 <https://creativecommons.org/licenses/by/2.0>, via Wikimedia Commons"
+            },
+            {
+                value: 135000,
+                name: "London Eye",
+                img: "https://upload.wikimedia.org/wikipedia/commons/d/d6/London-Eye-2009.JPG",
+                attribution: "Khamtran, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 137000,
+                name: "The Great Pyramid of Giza",
+                img: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg",
+                attribution: "Nina at the Norwegian bokmål language Wikipedia, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons"
+            },
+            {
+                value: 157000,
+                name: "Cologne Cathedral",
+                img: "https://upload.wikimedia.org/wikipedia/commons/4/41/Cologne_Cathedral_and_the_Hohenzollern_Bridge.jpg",
+                attribution: "Jiuguang Wang, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 348000,
+                name: "Ayers Rock",
+                img: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Ayers-Rock.jpg",
+                attribution: "Australien-Links.ch, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 443200,
+                name: "Empire State Building",
+                img: "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+                attribution: "Sam Valadi, CC BY 2.0 <https://creativecommons.org/licenses/by/2.0>, via Wikimedia Commons"
+            },
+            {
+                value: 828000,
+                name: "Burj Khalifa",
+                img: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/1200px-Burj_Khalifa.jpg",
+                attribution: "Creative Commons Attribution-ShareAlike 3.0"
+            },
+            {
+                value: 979000,
+                name: "Angel Falls",
+                img: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Angel_Falls_in_Venezuela.jpg",
+                attribution: "Luis Carillo, Venezuela, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 3776000,
+                name: "Mount Fuji",
+                img: "https://upload.wikimedia.org/wikipedia/commons/9/95/Lake_Kawaguchiko_Sakura_Mount_Fuji_3.JPG",
+                attribution: "Midori, CC BY 3.0 <https://creativecommons.org/licenses/by/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 8848000,
+                name: "Mount Everest",
+                img: "https://upload.wikimedia.org/wikipedia/commons/1/15/Mt._Everest_from_Gokyo_Ri_November_5%2C_2012.jpg",
+                attribution: "Rdevany, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 3476000000,
+                name: "Moon diameter",
+                img: "https://upload.wikimedia.org/wikipedia/commons/e/e1/FullMoon2010.jpg",
+                attribution: "Gregory H. Revera, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons"
+            },
+            {
+                value: 4880000000,
+                name: "Mercury diameter",
+                img: "https://upload.wikimedia.org/wikipedia/commons/b/b9/3D_Mercury.png",
+                attribution: "Jcpag2012, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+            },
+            {
+                value: 6792000000,
+                name: "Mars diameter",
+                img: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Plan%C3%A8te_Mars.jpg",
+                attribution: "Tris1606, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons"
+            }
+        ]
+    }
+}
---
+++ js/scripts.js
@@ -0,0 +1,413 @@
+// const bankNotesData = require.main.require('./data/bankNotesData.json');
+
+
+const formatToNumber = (text) => {
+    const value = text.replace(/\D/g, '');
+    return parseInt(value).toLocaleString("en-US");
+}
+
+const autoFormatToNumber = (event) => {
+    event.target.value = formatToNumber(event.target.value);
+};
+
+
+/**
+ * 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm
+ */
+function getWidthBankNote(currency, value) {
+    return bankNotesData[currency]["bankNotes"][value].width * 10;
+}
+
+/**
+ * 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm
+ */
+function getHeightBankNote(currency, value) {
+    return bankNotesData[currency]["bankNotes"][value].height * 10;
+}
+
+
+
+/**
+ * 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns grams
+ */
+function getWeightBankNote(currency, value) {
+    return bankNotesData[currency]["bankNotes"][value].weight;
+}
+
+
+/**
+ * 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm
+ */
+function getThicknessBankNote(currency, value) {
+    return bankNotesData[currency]["bankNotes"][value].thickness;
+}
+
+
+/**
+ * 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm^3
+ */
+function getVolume(currency, value) {
+    const bankNoteWidth = getWidthBankNote(currency, value);
+    const bankNoteHeight = getHeightBankNote(currency, value);
+    const bankNoteThickness = getThicknessBankNote(currency, value);
+
+    return bankNoteWidth * bankNoteHeight * bankNoteThickness;
+
+}
+
+/**
+ * 
+ * @param {*} cash 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm
+ */
+function CashToHeight(cash, currency, value) {
+    const bankNoteThickness = getThicknessBankNote(currency, value);
+    const numberNotes = Math.ceil(cash / value);
+
+    return numberNotes * bankNoteThickness;
+}
+
+/**
+ * 
+ * @param {*} cash 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm^3
+ */
+function CashToVolume(cash, currency, value) {
+    const bankNoteVolume = getVolume(currency, value);
+
+    const numberNotes = Math.ceil(cash / value);
+
+    return bankNoteVolume * numberNotes;
+}
+
+/**
+ * 
+ * @param {*} cash 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns g
+ */
+function CashToWeight(cash, currency, value) {
+    const bankNoteThickness = getWeightBankNote(currency, value);
+    const numberNotes = Math.ceil(cash / value);
+
+    return numberNotes * bankNoteThickness;
+}
+
+/**
+ * 
+ * @param {*} cash 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm
+ */
+function CashToCubeSize(cash, currency, value) {
+    return Math.cbrt(CashToVolume(cash, currency, value))
+}
+
+
+/**
+ * 
+ * @param {*} cash 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns mm
+ */
+function CashToSideBySideLength(cash, currency, value) {
+    const bankNoteWidth = getWidthBankNote(currency, value);
+
+    const numberNotes = Math.ceil(cash / value);
+
+    return numberNotes * bankNoteWidth;
+}
+
+/**
+ * Returns the time needed to receive the amount of cash if someone gives you 1 banknote /sec
+ * @param {*} cash 
+ * @param {*} currency 
+ * @param {*} value 
+ * @returns s
+ */
+function CashToTime(cash, currency, value) {
+    const numberNotes = Math.ceil(cash / value);
+    return numberNotes;
+}
+
+
+function CashToYears(cash, cashDaily = 100) {
+    const days = Math.floor(cash / cashDaily);
+    const years = days / 365.25;
+    return (years);
+}
+
+function CashToLife(cash, cashDaily, lifeDuration = 80) {
+    const years = CashToYears(cash, cashDaily);
+    return years / lifeDuration;
+}
+
+
+function round2decimals(n) {
+    return Math.round(n * 100) / 100;
+}
+
+
+function loadData() {
+    const amount = document.getElementById("amount").value.replace(/\D/g, '');
+
+    const currenciesRadio = document.getElementsByName("currencies");
+
+
+    let currency;
+
+    for (var i = 0, length = currenciesRadio.length; i < length; i++) {
+        if (currenciesRadio[i].checked) {
+            // do whatever you want with the checked radio
+            currency = currenciesRadio[i].value;
+
+            // only one radio can be logically checked, don't check the rest
+            break;
+        }
+    }
+
+
+    // const valuesSelect = document.getElementById("value");
+    const valuesSelect = prevNote.firstChild.innerHTML;
+    const value = valuesSelect.replace(/^\D+/g, '');
+    // console.log(value);
+
+    // const value = valuesSelect.options[valuesSelect.selectedIndex].text.replace(/^\D+/g, '');
+
+    // console.log(amount, currency, value)
+
+    document.getElementById("volume").innerHTML = formatBestUnitLength(CashToCubeSize(amount, currency, value));
+    document.getElementById("height").innerHTML = formatBestUnitLength(CashToHeight(amount, currency, value));
+    document.getElementById("weight").innerHTML = round2decimals(CashToWeight(amount, currency, value) / 1000) + " kg";
+    document.getElementById("length").innerHTML = formatBestUnitLength(CashToSideBySideLength(amount, currency, value));
+    document.getElementById("years").innerHTML = round2decimals(CashToYears(amount, value)) + " years";
+
+}
+
+
+
+
+function resizeInput() {
+    this.style.width = (this.value.length + 0.5) + "ch";
+}
+
+
+function UpdateSelect() {
+
+    if (prevCurrency) {
+        const currency = prevCurrency.value;
+        const selectCurrency = document.getElementById("value");
+
+        const selectCurrencyDiv = document.getElementById("select_values");
+
+
+
+        // We remove previous options
+        while (selectCurrencyDiv.firstChild) {
+            selectCurrencyDiv.removeChild(selectCurrencyDiv.firstChild);
+        }
+
+        // We create dynamically the new ones
+        const bankNotesCurrency = bankNotesData[prevCurrency.value]["bankNotes"];
+        const currencySymbol = bankNotesData[prevCurrency.value]["symbol"];
+
+        for (const note in bankNotesCurrency) {
+            const valueNote = bankNotesCurrency[note]["value"];
+            const selectNote = document.createElement('option');
+
+            const noteOption = document.createElement('span');
+            noteOption.id = `${valueNote}_${currency}`;
+            noteOption.className = "noteBank";
+            noteOption.addEventListener("click", UpdateNote);
+
+            const noteValue = document.createElement('span');
+            noteValue.innerHTML = `${currencySymbol}${valueNote}`;
+            noteValue.className = "noteValue";
+            noteOption.title = `${currencySymbol}${valueNote}:\n   width: ${bankNotesCurrency[note]["width"]}cm\n   height: ${bankNotesCurrency[note]["height"]}cm\n   thickness: ${bankNotesCurrency[note]["thickness"]}mm\n   weight: ${bankNotesCurrency[note]["weight"]}g`;
+
+            noteOption.appendChild(noteValue);
+            selectCurrencyDiv.appendChild(noteOption);
+        }
+    }
+}
+
+function UpdateSymbolAmount() {
+
+    if (prevCurrency) {
+        // We change symbol on amount
+        const fontAwesome = bankNotesData[prevCurrency.value]["fontAwesome"];
+        const amount = document.getElementById("amount_sign");
+        amount.classList = [fontAwesome];
+        const background = document.getElementsByClassName("deco_amount");
+        for (const b of background) {
+            // b.innerHTML = bankNotesData[prevCurrency.value]["symbol"];
+
+            b.setAttribute('data-after', bankNotesData[prevCurrency.value]["symbol"]);
+
+        }
+    }
+
+}
+
+
+let prevCurrency = null;
+
+function UpdateCurrency(r) {
+    if (r !== prevCurrency) {
+        prevCurrency = r;
+
+        UpdateSelect();
+        UpdateSymbolAmount();
+    }
+}
+
+
+let prevNote = null;
+
+function UpdateNote() {
+    if (this !== prevNote) {
+        if (prevNote) {
+            prevNote.classList.remove("selected_note");
+        }
+        prevNote = this;
+
+        this.classList.add("selected_note");
+
+        const background = document.getElementsByClassName("deco_amount");
+        for (const b of background) {
+
+            const valuesSelect = prevNote.firstChild.innerHTML;
+            const value = valuesSelect.replace(/^\D+/g, '');
+            b.innerHTML = value;
+        }
+
+
+        // UpdateSelect();
+        // UpdateSymbolAmount();
+    }
+}
+
+function showItem(itemData) {
+    const itemLi = document.createElement('li');
+    const itemValue = document.createElement('span');
+    const itemName = document.createElement('span');
+    const itemImg = document.createElement('img');
+
+    itemValue.innerHTML = formatBestUnitLength(itemData.value);
+    itemName.innerHTML = itemData.name;
+    itemImg.src = itemData.img;
+    itemImg.title = itemData.attribution;
+    itemImg.className = "comparison_img";
+
+    itemLi.appendChild(itemName);
+    itemLi.appendChild(itemValue);
+    itemLi.appendChild(itemImg);
+
+    return itemLi;
+}
+
+
+function formatBestUnitLength(length) {
+    let bestUnit = valueComparison.length.baseUnit;
+    let bestUnitRatio = 1;
+    const units = valueComparison.length.unit;
+    for (unit in units) {
+        if (units[unit] > length)
+            break;
+        bestUnit = unit;
+        bestUnitRatio = units[unit];
+    }
+
+    return Math.floor(100 * length / bestUnitRatio) / 100 + " " + bestUnit;
+}
+
+window.onload = function() {
+
+
+    const amountInput = document.getElementById("amount");
+
+    // amountInput.addEventListener('keydown', enforceFormat);
+    amountInput.addEventListener('keydown', autoFormatToNumber);
+    amountInput.addEventListener('keyup', autoFormatToNumber);
+    amountInput.addEventListener('input', resizeInput); // bind the "resizeInput" callback on "input" event
+    resizeInput.call(amountInput); // immediately call the function
+
+
+    console.log(CashToWeight(1000000000, "euro", 100));
+
+    $.getJSON('https://forbes400.herokuapp.com/api/forbes400?limit=10', function(data) {
+        // JSON result in `data` variable
+
+        ul = document.createElement('ul');
+        document.getElementById('top10').appendChild(ul);
+
+
+        let i = 0;
+        for (const p of data) {
+
+            let li = document.createElement('li');
+            ul.appendChild(li);
+
+            let button = document.createElement('div');
+
+            let span = document.createElement('span');
+            button.type = "button";
+            button.className = "topList";
+            span.innerHTML = `${p.person.name}`;
+            button.title = `Select ${p.person.name} ($${Math.floor(p.estWorthPrev/1000)} B)`;
+            button.name = `top_${i}`;
+            i++;
+
+            button.onclick = function() {
+                document.getElementById("amount").value = formatToNumber("" + Math.floor(data[this.name.split("_")[1]].estWorthPrev * 1000000));
+                // console.log(Math.floor(data[this.name.split("_")[1]].estWorthPrev * 1000000));
+                document.getElementById("currency_dollar").checked = true;
+                resizeInput.call(amountInput);
+
+                prevCurrency = document.getElementById("currency_dollar");
+                UpdateSelect();
+                UpdateSymbolAmount();
+                prevNote = null;
+            };
+            li.appendChild(button);
+            button.appendChild(span);
+        }
+
+        //li.innerHTML += `${p.person.name} ($${Math.floor(p.estWorthPrev/1000)} B)` //(${}) ;
+
+
+
+        console.log(data);
+        // document.writeln(p.person.name) console.log(p.person.name);
+
+    });
+
+    // const listItem = document.createElement("ul");
+    // for (const item of valueComparison.length.comparison) {
+    //     listItem.appendChild(showItem(item));
+    // }
+    // formatBestUnitLength(10);
+    // document.getElementById("listItems").appendChild(listItem);
+};
